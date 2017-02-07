5a149e52da84b2db31cf5b7b71168cae53fa6144
carlosflrs <carlos.flrsarlln@gmail.com>
2017-02-07 10:39:27 -0800

    Added Alg. Complexity lecture slides.

--- assets/schedule.js
+++ assets/schedule.js
@@ -127,7 +127,9 @@ cs10.week4 = {
                 'https://medium.com/@_marcos_otero/the-real-10-algorithms-that-dominate-our-world-e95fa9f16c04',
                 'optional')
     ],
-    lect1: lect('Algorithmic Complexity'),
+    lect1: lect('Algorithmic Complexity',
+            'https://drive.google.com/open?id=0B4KuCtIkhB7QUGYwbmg3a1B5X0E'
+    ),
     lab1: lab('Algorithmic Complexity', 'berkeley_bjc/areas/algorithm-complexity.topic', true),
     lect2: lect('Mutability'),
     lab2: lab('HW2 Work Session'),
